e6209aba8ae3f44f2ccda8f10b863848a661b2cb
Afonso Julio Moreira Neto <afonso.moreira@sensedia.com>
2025-02-21 09:49:13 -0300

    Remoção de apólice pré gerado pelo Before()

--- src/features/hooks/hooks.js
+++ src/features/hooks/hooks.js
@@ -1,21 +1,9 @@
 const { Before } = require("@cucumber/cucumber");
 const apoliceService = require("../../../services/rnpa-auto-v2/apolices");
-const apoliceRequestBody = require("../../../models/request/POST_apolices/apolices_request_body");
+const apoliceRequestBody = require("../../../models/request/POST_apolices/apolices_request_body").apoliceIndividual();
 
 
 Before(async (scenario) => {
   console.log("\n" + "_".repeat(scenario.pickle.name.length + 50));
   console.log(`\n === ${scenario.pickle.name} ===`);
 });
-
-Before({tags: 'not @post-apolices'}, async function () {
-  
-  if(process.env.NODE_ENV == "local" || process.env.NODE_ENV == "dev") {
-    const rq = apoliceRequestBody();
-
-    this.preGeneratedApolice = await apoliceService.postApolices({ requestBody:  rq});
-  
-    console.log("Apólice gerada previamente armazenada na variável 'this.preGeneratedApolice'");
-    console.log(rq.dadosDaApolice.numeroApolice);
-  }
-})
